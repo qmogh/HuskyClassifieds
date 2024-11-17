94358395bddd68b92c6f67cc532eabd8169e70bd
Amogh Chaubey <chaubeyamogh@gmail.com>
2024-11-17 01:27:19 -0500

    AUTH IS WORKING!!! SIGN IN IS WORKING WITH EMAIL :)))

---
+++ src/auth.config.ts
@@ -0,0 +1,19 @@
+import NodeMailer from "next-auth/providers/nodemailer";
+
+import type { NextAuthConfig } from "next-auth";
+
+export default {
+    providers: [NodeMailer({
+        id: 'email',
+        name: 'email',
+        server: {
+            host: process.env.EMAIL_SERVER_HOST,
+            port: process.env.EMAIL_SERVER_PORT,
+            auth: {
+                user: process.env.EMAIL_SERVER_USER,
+                pass: process.env.EMAIL_SERVER_PASSWORD
+            }
+        },
+        from: process.env.EMAIL_FROM,
+    })],
+} satisfies NextAuthConfig
---
+++ src/auth.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter"; 
+import { prisma } from "@/lib/database"
+import authConfig from "./auth.config";
+
+export const {
+    handlers: {GET, POST},
+    auth, 
+    signIn, 
+    signOut
+} = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    session: {
+        strategy: "jwt",
+    },
+    ...authConfig
+})
