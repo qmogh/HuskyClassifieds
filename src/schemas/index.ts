1ead0d8a8bedc95f22a5bbf271bec808910dd454
Amogh Chaubey <chaubeyamogh@gmail.com>
2024-12-06 13:48:17 -0500

    temporarily remove uconn email requirements

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -4,12 +4,12 @@ export const LoginSchema = z.object({
   email: z
     .string()
     .email({ message: 'Please enter a valid email address' })
-    .refine(
-      (email) => email.endsWith('@uconn.edu') || email === 'chaubeyamogh@gmail.com',
-      {
-        message: 'Email must end with @uconn.edu',
-      }
-    ),
+    // .refine(
+    //   (email) => email.endsWith('@uconn.edu') || email === 'chaubeyamogh@gmail.com',
+    //   {
+    //     message: 'Email must end with @uconn.edu',
+    //   }
+    // ),
 });
 
 // export const RegisterSchema = z.object({
