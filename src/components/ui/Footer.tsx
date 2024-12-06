693834b4d973e34de3785e24e93d4c0da189c21a
Amogh Chaubey <chaubeyamogh@gmail.com>
2024-12-06 15:43:14 -0500

    Added a cute little footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -2,7 +2,8 @@ export default function Footer() {
     return (
       <footer className="bg-[#0e1837] text-white p-4 mt-8">
         <div className="container mx-auto text-center">
-          <p>&copy; 2024 Husky Classifieds. All rights reserved.</p>
+          <p>&copy; 2024 Husky Classifieds. Made with <a href="https://amogh.sh">♡</a> & <a href="https://github.com/qmogh/huskyhousing">&lt;&gt;</a> in Storrs</p>
+
         </div>
       </footer>
     )
