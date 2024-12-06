c66abf250104e42bcb07e6f7e6cbb6efebf4ef4c
Amogh Chaubey <chaubeyamogh@gmail.com>
2024-12-06 14:25:36 -0500

    oops the footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
       <footer className="bg-[#0e1837] text-white p-4 mt-8">
         <div className="container mx-auto text-center">
-          <p>&copy; 2024 Husky Housing. All rights reserved.</p>
+          <p>&copy; 2024 Husky Classifieds. All rights reserved.</p>
         </div>
       </footer>
     )
