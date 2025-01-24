244c61d2633364e5f3daf3e7e65b24b6c8ce1f47
Amogh Chaubey <chaubeyamogh@gmail.com>
2025-01-24 13:48:26 -0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ const about = () => {
     return(
         <>        
         <p>Hi I am Amogh and I made this website!</p>
-        <p> I didn't make the thrift den but I can make an about page for them</p>
+        <p> I did not make the thrift den but I can make an about page for them</p>
         </>
     )
 }
